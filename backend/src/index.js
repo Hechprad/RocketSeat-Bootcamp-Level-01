29bf68103ec5974b77e69f0a90969fb7c0404481
Jorge Hecherat <hecherat@gmail.com>
2020-04-25 16:51:49 -0300

    [Backend] - First routes added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,66 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
+app.use(express.json());
+
+const projects = [];
+
 app.get("/projects", (request, response) => {
-  return response.send({ message: "Hello World" });
+  // const { title, name } = request.query;
+
+  // console.log(title);
+  // console.log(name);
+
+  return response.json(projects);
 });
 
-app.listen(3333);
+app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found." });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found." });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.listen(3333, () => {
+  console.log("🚀 Back-end started!");
+});
