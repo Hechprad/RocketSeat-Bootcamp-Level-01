79a8adbba1eb71f4e2dd2c2cad26b08b6fe416dd
Jorge Hecherat <hecherat@gmail.com>
2020-04-25 17:52:05 -0300

    [Backend] - Middleware function added to validate the project id at the POST request

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,34 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next(); //Próximo Middleware
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid project ID" });
+  }
+  return next();
+}
+
+app.use(logRequests);
+app.use("/projects/:id", validateProjectId);
 
 app.get("/projects", (request, response) => {
   const { title } = request.query;
