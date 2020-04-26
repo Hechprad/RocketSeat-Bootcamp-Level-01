57f8f67cb880ecb187a4d5a41772c0de029d8991
Jorge Hecherat <hecherat@gmail.com>
2020-04-26 18:51:04 -0300

    [Frontend] - Props and state added to Header component as example

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import Header from "./components/Header";
+
+function App() {
+  const [projects, setProjects] = useState([
+    "Desenvolvimento de app",
+    "Front-end web",
+  ]);
+
+  const handleAddProject = () => {
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+  };
+
+  return (
+    <>
+      <Header title="Projects" />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={() => handleAddProject()}>
+        Adicionar projeto
+      </button>
+    </>
+  );
+}
+
+export default App;
